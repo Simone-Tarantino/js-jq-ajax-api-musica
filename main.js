bf51fa9dce3f7e9294ab08b9a2a35b641ab26258
Simone Tarantino <tarantinosimonew@gmail.com>
2020-01-31 18:48:05 +0100

    Esercizio completo

--- main.js
+++ main.js
@@ -2,11 +2,23 @@ $(document).ready(function() {
 	$.ajax({
     'url' : 'https://flynn.boolean.careers/exercises/api/array/music',
     'method' : 'GET',
-    'success' : function() {
-
+    'success' : function(data) {
+      getData(data.response);
     },
-    'error' : function() {
-      
+    'error' : function(request, state, errors) {
+      alert('Errore ' + errors);
     }
   });
+
+  function getData (cds) {
+    for (var i = 0; i < cds.length; i++) {
+      var singleCd = cds[i];
+      var source = $("#entry-template").html();
+      var template = Handlebars.compile(source);
+      var html = template(singleCd);
+
+      $('.cds-container').append(html);
+    }
+  }
+
 });
