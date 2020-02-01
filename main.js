4dfa4d5c9be579c95a77e060d65c1bbeb19f9016
Simone Tarantino <tarantinosimonew@gmail.com>
2020-02-01 15:48:58 +0100

    Bonus completo

--- main.js
+++ main.js
@@ -4,11 +4,18 @@ $(document).ready(function() {
     'method' : 'GET',
     'success' : function(data) {
       getData(data.response);
+			$(document).on('click', '.genre-selection option', function(){
+	      var selection = $('option:selected').val();
+	      console.log(selection);
+				getDataGenre(data.response, selection);
+	    });
     },
     'error' : function(request, state, errors) {
       alert('Errore ' + errors);
     }
   });
+
+	// function per stampare tutti i cd
 
   function getData (cds) {
     for (var i = 0; i < cds.length; i++) {
@@ -21,4 +28,18 @@ $(document).ready(function() {
     }
   }
 
+	// function per stampare solo cd con determinato genere
+
+	function getDataGenre (cds, genre) {
+		$('.cd').hide();
+    for (var i = 0; i < cds.length; i++) {
+      var singleCd = cds[i];
+			if (genre == singleCd.genre){
+				var source = $("#entry-template").html();
+				var template = Handlebars.compile(source);
+				var html = template(singleCd);
+				$('.cds-container').append(html);
+			}
+    }
+  }
 });
